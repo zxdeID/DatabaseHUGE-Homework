fa9ec83167ca13693f727ab59efa29e59beca61b
mmmmmmsy <104258557+mmmmmmsy@users.noreply.github.com>
2022-11-02 00:31:49 +0800

    Update queryFunction.py
    增加了两个查询语句（虽然其中一个与原本想法不太一致）

--- queryFunction.py
+++ queryFunction.py
@@ -57,10 +57,52 @@ def query2(db):
     return
 
 def query3(db):
-    pass
+    '''
+    给定疾病，查询该疾病的患病人数
+    db:连接的数据库
+    小bug：也没有考虑空结果
+    之前的想法要实现略有些麻烦，于是做了点改变（如果脑子还能活下来，就再试试看之前的想法）
+    '''
+    os.system('cls')
+    dise = input('请输入疾病名称:\n')
+    cursor = db.cursor()
+    sql = '''
+    select COUNT(*) AS scount
+    from disease_suffered D, people P
+    where D.diseaseGet = '{}' and D.hid = P.hid
+    '''.format(dise)
+    cursor.execute(sql)
+    ret = cursor.fetchall()
+    os.system('cls')
+    print('the number of patients:{}'.format(ret))
+    a = input('输入任意键回车返回上一级菜单')
+    return
 
 def query4(db):
-    pass
+    '''
+    给定疾病，查询患有该病的患者的平均年龄
+    db:连接的数据库
+    小bug:依旧没排除空结果
+    '''
+    os.system('cls')
+    dise = input('请输入疾病名称:\n')
+    cursor = db.cursor()
+    sql = '''
+    select AVG(P.age)
+    from people P
+    where exists
+    (select *
+    from disease_list DL
+    where DL.name = '{}' and DL.hid = P.hid
+    )
+    '''.format(dise)
+    cursor.execute(sql)
+    ret = cursor.fetchall()
+    os.system('cls')
+    print('average age:{}'.format(ret))
+    a = input('输入任意键回车返回上一级菜单')
+    return
+
 
 def query5(db):
     pass
