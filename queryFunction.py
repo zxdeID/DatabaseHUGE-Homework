33744e864e33a349f07cca2fc7beea5cf15083ec
Lwind233 <1364372889@qq.com>
2022-11-10 23:47:04 +0800

    语句5还不能使用，别的都能用了(除了点小bug)

--- queryFunction.py
+++ queryFunction.py
@@ -110,43 +110,96 @@ def query5(db):
     '''
     给定年龄或职业 查找患者患有的疾病有哪些
     db:连接的数据库
+    bug还有很多还没弄好
     '''
-    os.system('cls')
-    # tableName = showTables(db, '查询')
-    # cols = showColumns(db, tableName)
-    # showTable(db, tableName)
+    # os.system('cls')
+    # data_work = input("请输入要查询的职业：\n")
+    # data_age = eval(input("请输入要查询的年龄：\n"))
 
-    print("请输入要查询的职业：")
-    data_work = input()
-    print("请输入要查询的年龄：")
-    data_age = eval(input())
-
-    cursor = db.cursor()  # 获 取 游 标
-    sql = '''
-    Select diseaseGet\
-    from people disease_suffered\
-    where people.hid=disease_suffered.hid and people.age={}\
-    intersect\
-    select diseaseGet\
-    from people disease_suffered\
-    where people.hid=disease_suffered.hid and people.work='{}'\
-    '''.format(data_age,data_work)
-    print(sql)
-
-    try:
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        for a in result:
-            print(f"该条件下患者患有的疾病有:{a}\n")
-    except:
-        print('查询失败')
-        traceback.print_exc()
-        time.sleep(30)
-        # 关闭光标对象
-        cursor.close()
+    # cursor = db.cursor()  # 获 取 游 标
+    # sql = '''
+    # select DS.diseaseGet\
+    # from people P, disease_suffered DS\
+    # where P.hid=DS.hid and P.age='{}'\
+    # intersect\
+    # select DS.diseaseGet\
+    # from people P, disease_suffered DS\
+    # where P.hid=DS.hid and P.work='{}'\
+    # '''.format(data_age,data_work)
+    # '''print(sql)'''
+    # cursor.execute(sql)
+    # ret = cursor.fetchall()
+    # os.system('cls')
+    # print("该条件下患者患有的疾病有:")
+    # for a in ret:
+    #     print(a)
     return
 
 def query6(db):
-    pass
+    '''
+        给定修改人查询修改时间和修改内容
+        db:连接的数据库
+    '''
+    os.system('cls')
+    data_editor = input("请输入修改人的名字\n")
+    cursor = db.cursor()  # 获 取 游 标
+    sql = '''select content , time  from edit_Record eR where eR.editor = '{}' '''.format(data_editor)
+    cursor.execute(sql)
+    ret = cursor.fetchall()
+    os.system('cls')
+    for a in ret:
+        content = a[0]
+        time = a[1]
+        print(f"修改内容为：{content}修改时间为:{time}")
+    a = input('输入任意键回车返回上一级菜单')
+    return
 
+def query7(db):
+    '''
+        给定疾病名称，查询病人的职业
+        db:连接的数据库
+        bug:sql语句有问题，会查询到重复的结果
+    '''
+    os.system('cls')
+    dise = input("请输入疾病名称\n")
+    cursor = db.cursor()
+    sql = '''
+    Select P.work
+    From people  P, disease_suffered D
+    Where P.hid = D.hid  and D.hid in(
+    Select F.hid
+    From disease_suffered F 
+    Where F.diseaseGet='{}')
+    '''.format(dise)
+    cursor.execute(sql)
+    ret = cursor.fetchall()
+    os.system('cls')
+    for i in ret:
+        print(i[0])
+    a = input('输入任意键回车返回上一级菜单')
+    return
+
+def query8(db):
+    '''
+        给定疾病名称，查询提供疾病资料的人的遗传病史
+        db:连接的数据库
+    '''
+    os.system('cls')
+    dise = input("请输入疾病名称\n")
+    cursor = db.cursor()
+    sql = '''
+    Select P.disease_history
+    From people P,disease_suffered D
+    Where D.hid=P.hid and D.hid in(
+    Select F.hid
+    From disease_suffered F 
+    Where F.diseaseGet='{}')
+    '''.format(dise)
+    cursor.execute(sql)
+    ret = cursor.fetchall()
+    os.system('cls')
+    for i in ret:
+        print(i[0])
+    a = input('输入任意键回车返回上一级菜单')
+    return
 
